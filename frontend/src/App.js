c76f50aba1be3e15b75a524a3f8aeb3ebdba7e0d
Daniel Cheong <dcheo001@gmail.com>
2022-02-15 21:47:20 -0500

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import "./app.css";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { useEffect, useState } from "react";
-import { Room, Star, StarBorder } from "@material-ui/icons";
+import { Room, Star } from "@material-ui/icons";
 import axios from "axios";
 import { format } from "timeago.js";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import Geocoder from 'react-mapbox-gl-geocoder';
 
 function App() {
   const myStorage = window.localStorage;
